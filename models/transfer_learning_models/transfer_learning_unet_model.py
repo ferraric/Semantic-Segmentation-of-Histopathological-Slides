61715000aac5bd46303d632d9f1fd1887c385195
PolyJerry <jeremy.scheurer@bluewin.ch>
2019-11-28 21:32:32 +0100

    upadeted few model parameters

--- models/transfer_learning_models/transfer_learning_unet_model.py
+++ models/transfer_learning_models/transfer_learning_unet_model.py
@@ -1,17 +1,28 @@
-import models.transfer_learning_models.transfer_learning_implementations as sm
 import tensorflow as tf
+SM_FRAMEWORK="tf.keras"
+import segmentation_models as sm
+sm.set_framework("tf.keras")
 
 
 class TransferLearningUnetModel():
     def __init__(self, config):
         self.config = config
-        if (self.config.use_backone_encoder_weights):
+        if (self.config.image_net_weights):
             self.encoder_weights = "imagenet"
         else:
             self.encoder_weights = None
+
+        if(self.config.number_of_classes == 2):
+            self.activation = 'sigmoid'
+        else:
+            self.activation = 'softmax'
         self.build_model()
 
 
     def build_model(self):
-        self.model = sm.Unet(backbone_name=self.config.backbone, classes=self.config.number_of_classes,
-                             encoder_weights=self.encoder_weights)
+        self.model = sm.Unet(backbone_name=self.config.backbone,
+                             input_shape=(self.config.image_size, self.config.image_size, 3),
+                             classes=self.config.number_of_classes,
+                             activation=self.activation,
+                             encoder_weights=self.encoder_weights,
+                             decoder_block_type=self.config.decoder_block_type)
