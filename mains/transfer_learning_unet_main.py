7473b15cc054447bc323f16708ef57d05e359779
PolyJerry <jeremy.scheurer@bluewin.ch>
2019-12-05 20:19:13 +0100

    added saving of predicitons labels and inputs to comet

--- mains/transfer_learning_unet_main.py
+++ mains/transfer_learning_unet_main.py
@@ -124,7 +124,7 @@ def main():
     experiment.log_asset(model_architecture_path)
     experiment.log_asset(args.config)
 
-    save_data(validation_dataloader, experiment, config)
+    save_input_label_and_prediction(model, validation_dataloader, experiment, config, 0, 0)
 
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate=config.learning_rate,
@@ -162,7 +162,7 @@ def main():
         metrics=[precision, recall, f1_score, matthews_corelation_coefficient, accuracy, mean_iou_with_argmax]
     )
 
-
+    return
     model.fit(train_dataloader.dataset, epochs=config.num_epochs, steps_per_epoch=len(train_dataloader), validation_data=validation_dataloader.dataset,
               validation_steps=len(validation_dataloader),
               callbacks=[EvaluateDuringTraningCallback(validate_every_n_steps=config.validate_every_n_steps, validation_dataloader=validation_dataloader,
@@ -170,50 +170,84 @@ def main():
               use_multiprocessing=False)
 
 
-def save_data(validation_dataloader, comet_experiment, config):
-    for i, el in enumerate(validation_dataloader.dataset):
-        assert el[1][0].numpy().shape == (config.image_size, config.image_size, config.number_of_classes)
-        im = el[0][0]
-        label = el[1][0]
-        np.save(os.path.join(config.summary_dir, "image.npy"), im.numpy())
-        np.save(os.path.join(config.summary_dir, "label.npy"), im.numpy())
+def save_input_label_and_prediction(model, validation_dataloader, comet_experiment, config, epoch, step):
+    for i, data in enumerate(validation_dataloader.dataset):
+        assert data[0][0].numpy().shape == (config.image_size, config.image_size, 3), data[0][0].numpy().shape
+        assert data[1][0].numpy().shape == (config.image_size, config.image_size, config.number_of_classes), data[1][0].numpy().shape
+        input = data[0][:1] #keep batch dimensions
+        label = data[1][0]
+        np.save(os.path.join(config.summary_dir, "image.npy"), input.numpy())
+        np.save(os.path.join(config.summary_dir, "label.npy"), label.numpy())
 
-        image = Image.fromarray(im.numpy().astype('uint8'), 'RGB')
-        comet_experiment.log_image(image)
+        input_np = input[0].numpy().astype('uint8')
+        assert input_np.shape == (config.image_size, config.image_size, 3), input_np.shape
+        input_image = Image.fromarray(input_np, 'RGB')
+
+        prediction = model.predict(input)
+        prediction_np = np.argmax(prediction[0], axis=-1).astype('uint8')
+        assert prediction_np.shape == (config.image_size, config.image_size), prediction_np.shape
+        prediction_image = Image.fromarray(prediction_np, "P")
+
         np_label = np.argmax(label.numpy(), -1).astype('uint8')
-        assert np_label.shape == (config.image_size, config.image_size)
-        label = Image.fromarray(np_label, 'P')
+        assert np_label.shape == (config.image_size, config.image_size), np_label.shape
+        label_image = Image.fromarray(np_label, 'P')
+
 
         if(config.number_of_classes == 3):
-            label.putpalette([
+            input_image.putpalette([
+                255, 255, 255,  # white
+                255, 0, 0,  # red
+                0, 0, 255  # blue
+            ])
+            prediction_image.putpalette([
+                255, 255, 255,  # white
+                255, 0, 0,  # red
+                0, 0, 255  # blue
+            ])
+            label_image.putpalette([
                 255, 255, 255,  # white
                 255, 0, 0,  # red
                 0, 0, 255  # blue
             ])
         elif(config.number_of_classes ==2):
-            label.putpalette([
+            prediction_image.putpalette([
+                255, 255, 255,  # white
+                0, 0, 0,  # black
+            ])
+            label_image.putpalette([
                 255, 255, 255,  # white
                 0, 0, 0,  # black
             ])
 
         else:
             raise NotImplementedError()
+        prediction_image.save("/Users/jeremyscheurer/Desktop/hey.png")
+        comet_experiment.log_image(input_image, name="input_image_epoch{}_step{}".format(epoch, step))
+        comet_experiment.log_image(prediction_image, name="prediction_image_epoch{}_step{}".format(epoch, step))
+        comet_experiment.log_image(label_image, name="label_image_epoch{}_step{}".format(epoch, step))
 
-        image.save(os.path.join(config.summary_dir, "image.png"))
-        label.save(os.path.join(config.summary_dir, "label.png"))
-        break
+        #image.save(os.path.join(config.summary_dir, "image.png"))
+        #label.save(os.path.join(config.summary_dir, "label.png"))
+        if(i == 3):
+            break
 
 class EvaluateDuringTraningCallback(tf.keras.callbacks.Callback):
     """Callback that evaluates on validation set during training i.e. not only at end of epoch."""
 
-    def __init__(self, validate_every_n_steps, validation_dataloader, comet_experiment):
+    def __init__(self, validate_every_n_steps, validation_dataloader, comet_experiment, config):
         super(EvaluateDuringTraningCallback, self).__init__()
         self.validate_every_n_steps = validate_every_n_steps
         self.validation_dataloader = validation_dataloader
         self.comet_experiment = comet_experiment
+        self.config = config
 
     def on_epoch_begin(self, epoch, logs=None):
         self.seen = 0
+        self.current_epoch = 1
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.seen = 0
+        self.current_epoch +=1
 
     def on_batch_end(self, batch, logs=None):
         logs = logs or {}
@@ -228,6 +262,9 @@ class EvaluateDuringTraningCallback(tf.keras.callbacks.Callback):
             print("Evaluation metrics", evaluation_metrics)
             for i in range(1, len(evaluation_metrics)):
                 self.comet_experiment.log_metric("callback_validation" + self.model.metrics[i - 1].name, evaluation_metrics[i])
+
+            save_input_label_and_prediction(self.model, self.validation_dataloader, self.comet_experiment, self.config,
+                                            self.current_epoch, self.seen)
 
 
 def get_training_augmentations(p=0.6):
