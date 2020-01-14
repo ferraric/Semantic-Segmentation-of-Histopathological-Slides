9a3c2fc8a89374eb2aeeae4db2c7f70f9a9f9b45
Claudio Ferrari <ferraric@student.ethz.ch>
2020-01-14 20:47:15 +0100

    added multiclass matthews

--- tools/calculate_result_metrics_from_predictions_norway.py
+++ tools/calculate_result_metrics_from_predictions_norway.py
@@ -1,12 +1,17 @@
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-import os
+import os, sys, inspect
 import argparse
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+
 from utils.jeremy_metrics import  F1Score, MatthewsCorrelationCoefficient
 import tensorflow.keras.metrics as tf_keras_metrics
 import tensorflow.keras.losses as tf_keras_losses
-
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
@@ -21,16 +26,9 @@ if __name__ == '__main__':
     recall = tf_keras_metrics.Recall()  # equivalent to sensitivity in the norway paper
     mean_iou = tf.metrics.MeanIoU(num_classes=2)
     f1_score = F1Score(num_classes=2, average='micro')  # dice similarity is equivalent to f1 score
-    matthews_corelation_coefficient = MatthewsCorrelationCoefficient()
+    matthews_corelation_coefficient = MatthewsCorrelationCoefficient(num_classes=2)
 
-    average_acc = 0
-    average_prec = 0
-    average_recall = 0
-    average_iou = 0
-    average_f1 = 0
-    average_matt = 0
-
-    # metrics
+    matt_sum = 0
 
     all_files = os.listdir(evaluation_folder)
     for file_name in all_files:
@@ -39,8 +37,10 @@ if __name__ == '__main__':
             print("looking at file {}".format(file_name))
             index = file_name.split("_")[1]
 
-            label = tf.dtypes.cast(tf.convert_to_tensor(np.array(Image.open(os.path.join(evaluation_folder, file_name)))), tf.float16)
-            prediction = tf.dtypes.cast(tf.convert_to_tensor(np.array(Image.open(os.path.join(evaluation_folder, "prediction_" + index)))), tf.float16)
+            label_np = np.array(Image.open(os.path.join(evaluation_folder, file_name)))
+            label = tf.dtypes.cast(tf.convert_to_tensor(label_np), tf.float16)
+            prediction_np = np.array(Image.open(os.path.join(evaluation_folder, "prediction_" + index)))
+            prediction = tf.dtypes.cast(tf.convert_to_tensor(prediction_np), tf.float16)
 
             accuracy.update_state(label, prediction)
             precision.update_state(label, prediction)
@@ -48,6 +48,7 @@ if __name__ == '__main__':
             mean_iou.update_state(label, prediction)
             f1_score.update_state(label, prediction)
             matthews_corelation_coefficient.update_state(label, prediction)
+
 
             print(accuracy.result(), precision.result(), recall.result(), f1_score.result(), mean_iou.result(), matthews_corelation_coefficient.result())
             print("\n")
